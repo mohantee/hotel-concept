8625c5ad41bb223ecd1c2b14463e6614445af518
Saurav Mohanty <sauravmohantee@gmail.com>
2023-12-09 15:38:41 +0530

    ci

--- server/config/mysql.config.ts
+++ server/config/mysql.config.ts
@@ -4,23 +4,20 @@ dotenv.config();
 import fs from "fs";
 import path from "path";
 
-const SEED_CONFIG = {
+const CONFIG = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   multipleStatements: true,
 };
 
+const connection = mysql.createPool(CONFIG);
+
 const seedFilePath = path.join(__dirname, "init.sql");
 const seedQuery = fs.readFileSync(seedFilePath, { encoding: "utf-8" });
-const seedConnection = mysql.createConnection(SEED_CONFIG);
 
-seedConnection.query(seedQuery);
-seedConnection.end();
-
-const DB_CONFIG = { ...SEED_CONFIG, database: process.env.DB_NAME };
-
-const connection = mysql.createPool(DB_CONFIG);
+connection.query(seedQuery);
 
 const pool = connection.promise();
 
